63033c0d46755b3eef98e49ffcea2b0fb0f97d65
midore <midorex@gmail.com>
2011-12-11 09:58:37 +0900

    deleted gsub

--- rm-dsstore.rb
+++ rm-dsstore.rb
@@ -3,12 +3,15 @@
 
 #---------------------------------------------------------------------
 # usage
+# Search files named .DS_Store in ENV['HOME']
+# ruby rm-dsstore.rb
+# ruby rm-dsstore.rb "rmf"
+# Other directory
 # ruby rm-dsstore.rb /Users/name/dir
-# ruby19 rm-dsstore.rb /Volumes/hdd/
 # ruby19 rm-dsstore.rb /Users/name/dir 'rmf'
 #---------------------------------------------------------------------
 # Mac OS X 10.7.2
-# ruby 1.9.3dev (2011-09-23 revision 33323) [x86_64-darwin11.2.0]
+# ruby 1.9.3p0 (2011-10-30 revision 33570) [x86_64-darwin11.2.0]
 # ruby 1.8.7 (2010-01-10 patchlevel 249) [universal-darwin11.0]
 
 require 'find'
@@ -27,20 +30,16 @@ class DSStore
     return print "It is not directory\n" unless @checkdir
     return print "not found .DS_Store file\n" if list.empty?
     @list.each{|f|
-      file = f =~ /\s/ ? f.gsub(/\s/, "\\ " ) : f
+      file = f =~ /\s/ ? f.gsub(/\s/, "\ " ) : f
       @option == 'rmf' ? sys_rm(file) : sys_ls(file)
     }
   end
 
   private
-
   def list
     Find.find(@path){|path|
      next unless File.readable?(path)
      next if File.directory?(path)
-     #next if path.include?("rurema")
-     #next if path.include?("MacVim")
-     #next if File.extname(path) == 'png'
      x = File.basename(path)
      @list << path if x == ".DS_Store"
     }
@@ -48,17 +47,15 @@ class DSStore
   end
 
   def sys_ls(f)
-    gf = f.gsub("&","*")
-    system("ls  #{gf}")
+    system("ls \'#{f}\'")
   end
 
   def sys_rm(f)
-    system("rm #{f}")
+    system("rm \'#{f}\'")
     print "Removed: #{f}\n"
   end
 end
 
-# run
 t = Time.now
 dir, opt = ENV['HOME'], nil if ARGV.nil?
 if ARGV[1].nil?
@@ -70,10 +67,8 @@ if ARGV[1].nil?
   end
 end
 dir, opt = ARGV[0], ARGV[1] unless ARGV[1].nil?
-
 print "TargetDir: #{dir}\nOption: #{opt}\n"
 print "------------------------------\n"
-
 DSStore.new(dir, opt).ls_or_rm
 t2 = Time.now - t
 print "\n=>Time: #{t2.to_s}\n"
